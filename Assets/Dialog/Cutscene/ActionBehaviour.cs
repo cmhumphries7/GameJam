f6db932de046baf69832493156f9cf95710246d8
rworker <32530793+rworker@users.noreply.github.com>
2023-01-12 23:45:43 -0800

    more cutscene stuff

--- Assets/Dialog/Cutscene/ActionBehaviour.cs
+++ Assets/Dialog/Cutscene/ActionBehaviour.cs
@@ -9,11 +9,36 @@ public class ActionBehavior : PlayableBehaviour
 {
 
     private PlayableDirector director;
+    private bool clipPlayed = false;
 
     public override void OnPlayableCreate(Playable playable)
     {
         director = (playable.GetGraph().GetResolver() as PlayableDirector);
 
+    }
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        if (!clipPlayed
+            && info.weight > 0f)
+        {
+
+            if (Application.isPlaying)
+            {
+                /* if (hasToPause)
+                 {
+                     pauseScheduled = true;
+                 }*/
+                PauseTimeline(director);
+            }
+
+            clipPlayed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            director.playableGraph.GetRootPlayable(0).SetSpeed(1d);
+        }
     }
 
 

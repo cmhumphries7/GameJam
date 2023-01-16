86dadc94b2875771b6d1f092ea9feef95efd8f7c
aburmadethis <ashleynburkart@gmail.com>
2023-01-16 15:46:09 -0800

    Added Switches for footsteps & plant growing sfx
    Added footstep sfx & plant grow sfx to place on animations (not currently set up to play in game but set up to place)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,13 +13,33 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID MECHANIC_PLANT_GROW = 507898685U;
         static const AkUniqueID PLAY_MAIN_START = 3817812628U;
+        static const AkUniqueID PLAYER_SOUNDS = 4083419649U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GROUNDTYPE
+        {
+            static const AkUniqueID GROUP = 2835351336U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+            } // namespace SWITCH
+        } // namespace GROUNDTYPE
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MAIN_SOUND = 2014706742U;
+        static const AkUniqueID MECHANIC_PLANT_GROW = 507898685U;
+        static const AkUniqueID PLAYER_SOUNDS = 4083419649U;
     } // namespace BANKS
 
     namespace BUSSES

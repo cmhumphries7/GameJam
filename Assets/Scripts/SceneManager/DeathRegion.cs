d8b449eb964801c5939d032e041c3334e1b4cbc4
rworker <32530793+rworker@users.noreply.github.com>
2023-01-18 21:47:15 -0800

    small fixes

--- Assets/Scripts/SceneManager/DeathRegion.cs
+++ Assets/Scripts/SceneManager/DeathRegion.cs
@@ -6,11 +6,28 @@ using UnityEngine.SceneManagement;
 public class DeathRegion : MonoBehaviour
 {
 
+    [SerializeField] Animator FadeInOut;
+    public AudioSource audioSource;
+    [SerializeField] public AudioClip deathClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("DeathScene");
+            audioSource.PlayOneShot(deathClip);
+            FadeInOut.SetTrigger("FadeOut");
+            StartCoroutine(WaitFadeComplete());
         }
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene("DeathScene");
+    }
+
+    IEnumerator WaitFadeComplete()
+    {
+        yield return new WaitForSeconds(1);
+        OnFadeComplete();
     }
 }
